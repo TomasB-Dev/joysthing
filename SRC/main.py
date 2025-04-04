11fd0ba81769b2f5f237db735920c217fbd9b764
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-04-04 20:29:48 -0300

    fix program close too fast

--- SRC/main.py
+++ SRC/main.py
@@ -1,9 +1,31 @@
 from pywinusb import hid
-#no significa que todos sean joysticks
-dispositivos = hid.HidDeviceFilter().get_devices()
+def handler(datos_raw):
+    print("Raw data:", datos_raw)
 
-for dispositivo in dispositivos:
-    dispositivo.open()
-    print(f"Nombre {dispositivo.product_name}")
-    print(f"Id Producto: {hex(dispositivo.product_id)}")
-    dispositivo.close()
+def Listar_Dispositivo():
+    """
+    lista los dispositivos y printea nombre y id del producto
+    """
+    dispositivos = hid.HidDeviceFilter().get_devices()
+
+    for dispositivo in dispositivos:
+        if hex(dispositivo.product_id) == "0x6":
+            try:
+                dispositivo.open()
+                print("*************************************")
+                print(f"Nombre {dispositivo.product_name}")
+                print(f"Id Producto: {hex(dispositivo.product_id)}")
+                dispositivo.set_raw_data_handler(handler)
+                input("Finalizar..")
+                print("************************************")
+                print(dispositivo.find_input_reports())
+                dispositivo.close() 
+            except Exception as e:
+                print(f"{e}")
+                
+            
+        else:
+            print("no se encontro")
+
+        
+Listar_Dispositivo()
