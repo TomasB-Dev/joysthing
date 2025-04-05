4f761c19198d18a994188563f73cfc3d18fa7c1c
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-04-04 21:39:46 -0300

    show btn press

--- SRC/main.py
+++ SRC/main.py
@@ -1,6 +1,14 @@
 from pywinusb import hid
 def handler(datos_raw):
+    """
+    mostrar info array de botones, place 6 contiene los btns del cuadrado, ciruclo, triangulo y x
+    """
     print("Raw data:", datos_raw)
+    boton_data = datos_raw[6]
+    for i in range(8):  # teoricamente hay 8 botones
+        if boton_data & (1 << i):
+            print(f"boton {i + 1} presionado")
+
 
 def Listar_Dispositivo():
     """
