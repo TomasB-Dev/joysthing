0b2ddd9a3bac792ffb5cad97b586ff700ce8fddc
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-04-05 09:53:58 -0300

    Update main.py

--- SRC/main.py
+++ SRC/main.py
@@ -3,7 +3,7 @@ import pyautogui
 last_join = [None]
 def handler(datos_raw):
     """
-    mostrar info array de botones, place 6 contiene los btns del cuadrado, ciruclo, triangulo y xxxxxxxxxx
+    mostrar info array de botones, place 6 contiene los btns del cuadrado, ciruclo, triangulo y x
     """
     
     if datos_raw[6] > 15 and datos_raw[3] < 115 or datos_raw[3] > 130 : #esto para controlar el spam de mi joystick no necesario
@@ -11,10 +11,11 @@ def handler(datos_raw):
     boton_data = datos_raw[6]
     if boton_data == 79 and last_join[0] != 79:
         last_join[0] = 79
-        pyautogui.press('x')
+        pyautogui.keyDown('x')
     #reasigna el estado de lastjoin
     elif boton_data != last_join[0]:
         last_join[0] = boton_data
+        pyautogui.keyUp('x')
 
 
 
